33679b90963272a808d317bfcdb6a08817872cfa
dantearaujo1 <danteeng@hotmail.com>
2022-10-04 16:39:34 -0300

    Writing in README and making more generic my plugin

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 This is my first attempt to create a plugin
 
-My problem is that i'm learning processing in my college and there's no
+My problem is that I'm learning processing in my college and there's no
 plugging to create the folder structure necessary to run processing sketch,
-so i made this simple plugin that you can create some kind of defaults for a project
+so I made this simple plugin that you can create some kind of defaults for a project
 inside neovim
 
 
 ## Requirements
 
-Nui.nvim - for floating windows.
+[Nui.nvim](https://github.com/munifTanjim/nui.nvim) - for floating windows.
--- lua/project-creator/init.lua
+++ lua/project-creator/init.lua
@@ -3,92 +3,109 @@ local Input = require("nui.input")
 
 local M = {}
 
+local menu_opts = {
+  position = "50%",
+  size = {
+    width = 80,
+    height = 10,
+  },
+  border = {
+    padding = {
+      top = 2,
+      right = 3,
+      bottom = 2,
+      left = 3,
+    },
+    style = "rounded",
+    text = {
+      top = "Create Project for: ",
+      top_align = "center",
+    },
+  },
+  buf_options = {
+    modifiable = true,
+    readonly = false,
+  },
+  win_options = {
+    winblend = 10,
+    winhighlight = "Normal:Normal,FloatBorder:FloatNormal",
+  },
+}
+
+local input_opts = {
+  position = {
+    row = "10%",
+    col = "50%",
+  },
+  size = {
+    width = 30,
+    height = 5,
+  },
+  border = {
+    padding = {
+      top = 2,
+      right = 3,
+      bottom = 2,
+      left = 3,
+    },
+    style = "rounded",
+    text = {
+      top = " Name of your project: ",
+      top_align = "center",
+    },
+  },
+  win_options = {
+    winhighlight = "Normal:Normal,FloatBorder:FloatNormal",
+  },
+}
+
+M.creation_table = {}
+M.setup = function(opts)
+  M.creation_table.projects = opts
+end
+
+M.setup({
+  processing = {
+    workspace = "~/dev/projects/processing",
+    cmd = "mkdir"
+  },
+  cpp_sfml = {
+    workspace = "~/dev/projects/cpp",
+    cmd = "mkdir"
+  },
+})
+
 M.first_upperCase = function(s)
   return (s:sub(1,1):upper() .. s:sub(2,string.len(s)))
 end
 
-M.creation_table = {
-  cmd = "! mkdir -p ",
-  projects = {
-    processing = {
-      workspace = "~/dev/projects/processing/"
-    },
-    cpp = {
-      workspace = "~/dev/projects/cpp/",
-    },
-  },
-}
+-- M.creation_table = {
+--   cmd = "! mkdir -p ",
+--   projects = {
+--     processing = {
+--       workspace = "~/dev/projects/processing",
+--       cmd = "mkdir -p "
+--     },
+--     cpp = {
+--       workspace = "~/dev/projects/cpp/",
+--     },
+--   },
+-- }
 
-M.items = {}
+M._items = {}
 
 M.create_items = function()
   for k, _ in pairs(M.creation_table.projects) do
-    table.insert(M.items,k)
+    table.insert(M._items,k)
   end
 end
 
 M.create_menu = function()
   local item_list = {}
 
-  for _, v in pairs(M.items) do
+  for _, v in pairs(M._items) do
     table.insert(item_list,Menu.item(M.first_upperCase(v)))
   end
-
-  local menu_opts = {
-    position = "50%",
-    size = {
-      width = 80,
-      height = 10,
-    },
-    border = {
-      padding = {
-        top = 2,
-        right = 3,
-        bottom = 2,
-        left = 3,
-      },
-      style = "rounded",
-      text = {
-        top = "Create Project for: ",
-        top_align = "center",
-      },
-    },
-    buf_options = {
-      modifiable = true,
-      readonly = false,
-    },
-    win_options = {
-      winblend = 10,
-      winhighlight = "Normal:Normal,FloatBorder:FloatNormal",
-    },
-  }
-
-  local input_opts = {
-    position = {
-      row = "10%",
-      col = "50%",
-    },
-    size = {
-      width = 30,
-      height = 5,
-    },
-    border = {
-      padding = {
-        top = 2,
-        right = 3,
-        bottom = 2,
-        left = 3,
-      },
-      style = "rounded",
-      text = {
-        top = " Name of your project: ",
-        top_align = "center",
-      },
-    },
-    win_options = {
-      winhighlight = "Normal:Normal,FloatBorder:FloatNormal",
-    },
-  }
 
   local menu = Menu(
     menu_opts,
@@ -98,18 +115,39 @@ M.create_menu = function()
       on_close = function()
         print("Menu Closed!")
       end,
-      on_submit = function(item)
-        if item.text == "Processing" then
-          local nmenu = Input(input_opts,{ lines ={}, on_close = {}, on_submit =
-            function(project)
-              vim.api.nvim_command(":! mkdir -p " .. M.creation_table.projects.processing.workspace .. "/" .. project )
-            end
-          })
-          nmenu:mount()
-        end
-      end
+      on_submit = M._on_submit_menu
   })
   menu:mount()
+end
+
+M._on_submit_menu = function(item)
+
+  local type = ""
+
+  for k,v in pairs(M.creation_table.projects) do
+
+    if item.text == M.first_upperCase(k) then
+      type = M.first_upperCase(k)
+    end
+
+  end
+
+  if item.text == type then
+
+    local nmenu = Input(input_opts,
+    {
+        lines ={},
+        on_submit =
+          function(project_name)
+            local project_table = M.creation_table.projects[string.lower(type)]
+            local path = project_table.workspace .. "/" .. project_name
+            vim.api.nvim_command(":! " .. project_table.cmd .. " " .. path)
+            vim.api.nvim_command(":! cd " .. path .. " && touch " .. project_name .. ".pde" )
+          end
+    })
+    nmenu:mount()
+
+  end
 end
 
 M.create_items()
@@ -117,106 +155,3 @@ M.create_items()
 
 return M
 
--- local menu_opts = {
---   position = "50%",
---   size = {
---     width = 80,
---     height = 10,
---   },
---   border = {
---     padding = {
---       top = 2,
---       right = 3,
---       bottom = 2,
---       left = 3,
---     },
---     style = "rounded",
---     text = {
---       top = "Create Project for: ",
---       top_align = "center",
---     },
---   },
---   buf_options = {
---     modifiable = true,
---     readonly = false,
---   },
---   win_options = {
---     winblend = 10,
---     winhighlight = "Normal:Normal,FloatBorder:FloatNormal",
---   },
--- }
--- local input_opts = {
---   position = {
---     row = "10%",
---     col = "50%",
---   },
---   size = {
---     width = 30,
---     height = 5,
---   },
---   border = {
---     padding = {
---       top = 2,
---       right = 3,
---       bottom = 2,
---       left = 3,
---     },
---     style = "rounded",
---     text = {
---       top = " Name of your project: ",
---       top_align = "center",
---     },
---   },
---   win_options = {
---     winhighlight = "Normal:Normal,FloatBorder:FloatNormal",
---   },
--- }
---
--- local menu = Menu(
---   menu_opts,
---  {
---   lines = {
---     Menu.item("Processing"),
---     Menu.item("Cpp"),
---     Menu.item("React"),
---     Menu.item("Other"),
---   },
---   max_width = 30,
---   on_close = function()
---     print("Menu Closed!")
---   end,
---   on_submit = function(item)
---     if item.text == "Processing" then
---         local nmenu = Input(input_opts,{lines ={}, on_close = {}, on_submit = {}})
---         nmenu:mount()
---     end
---   end
--- })
---
--- -- menu:mount()
---
--- local M = {}
---
--- M.setup = function(opts)
---   print("Options: ", opts)
--- end
---
--- M.create = function()
---   -- vim.ui.select({'processing','cpp'},{
---   --   prompt = 'Select a project to create: ',
---   --   format_item = function(item)
---   --     return "I'd like to create a " .. item
---   --   end,
---   -- }, function(choice)
---   --   if choice == 'processing' then
---   --     vim.ui.input({prompt = 'Enter your project name: '}, function(input)
---   --       print(string.format("\n Create project files for processing as: ") .. input)
---   --     end
---   --     )
---   --   else
---   --     print("Create another process")
---   --   end
---   -- end)
--- end
---
--- return M
